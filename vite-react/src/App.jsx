ca3b2d8a0c3932df70f30ae2e84cdee057f40e48
Aniket Kumar <aa6443@srmist.edu.in>
2024-07-17 09:45:45 +0530

    react first basic project

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,16 @@
+import Main from "./components/Main"
+import Navbar from "./components/Navbar"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+}
+
+export default App
